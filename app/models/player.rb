6df6a0d5d6d133a3bc9fda58e39d3020360bc987
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-09-21 17:04:20 +0200

    Did relations between models

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,4 @@
 class Player < ActiveRecord::Base
+	has_many :registrations
+	has_many :tournaments, through: :registrations
 end
--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,2 +1,4 @@
 class Tournament < ActiveRecord::Base
+	has_many :registrations
+	has_many :players, through: :registrations
 end
