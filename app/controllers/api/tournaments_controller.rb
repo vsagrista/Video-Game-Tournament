af029af501160227257c15c378eda073df0281d5
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-09-21 18:03:31 +0200

    New iteration: made create action on API controller

--- app/controllers/api/tournaments_controller.rb
+++ app/controllers/api/tournaments_controller.rb
@@ -3,4 +3,8 @@ class Api::TournamentsController < ApplicationController
   	tournaments = Tournament.all
     render json: tournaments
   end
+  def create
+  	tournament = Tournament.create(name: params[:tournament][:name])
+  	render json: tournament, status: 201
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'tournaments#index'
-namespace :api do 
+  namespace :api do 
     resources :tournaments, :players
   end
 end
